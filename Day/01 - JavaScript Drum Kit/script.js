1b6757625185454bb693e14e5d25f266872b67e0
plskz <zaisantillan14@gmail.com>
2021-04-07 20:50:55 +0800

    update day 1

---
+++ Day/01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,19 @@
+function removeTransition(e) {
+    if (e.propertyName !== "transform") return;
+    e.target.classList.remove("playing");
+}
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (audio) {
+        key.classList.add("playing");
+        audio.currentTime = 0;
+        audio.play();
+    }
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", playSound);
