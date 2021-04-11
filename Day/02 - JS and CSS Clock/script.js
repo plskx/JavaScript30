4a3ada69c58d4cb25c548ee94e565efc88fbf805
plskz <zaisantillan14@gmail.com>
2021-04-11 22:24:42 +0800

    Update Day 2

--- Day/02 - JS and CSS Clock/script.js
+++ Day/02 - JS and CSS Clock/script.js
@@ -1,26 +1,15 @@
-const secondHand = document.querySelector(".second-hand");
-const minHand = document.querySelector(".min-hand");
-const hourHand = document.querySelector(".hour-hand");
+const deg = 6;
+const hr = document.querySelector("#hr");
+const mn = document.querySelector("#mn");
+const sc = document.querySelector("#sc");
 
-function setDate() {
-  const now = new Date();
-  const seconds = now.getSeconds();
-  const secondsDegrees = (seconds / 60) * 360 + 90;
-  
-  console.log(seconds);
-  secondHand.style.transition = (seconds === 0) ? "none" : "all 0.05s ubic-bezier(0.1, 2.7, 0.58, 1)";
+setInterval(() => {
+  const day = new Date();
+  const hh = day.getHours() * 30;
+  const mm = day.getMinutes() * deg;
+  const ss = day.getSeconds() * deg;
 
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-
-  const mins = now.getMinutes();
-  const minsDegrees = (mins / 60) * 360 + 90;
-  minHand.style.transform = `rotate(${minsDegrees}deg)`;
-  minHand.style.transition = (mins === 0) ? "none" : "all 0.05s ubic-bezier(0.1, 2.7, 0.58, 1)"
-
-  const hour = now.getHours();
-  const hoursDegrees = (hour / 12) * 360 + 90;
-  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-  hourHand.style.transition = (hour === 0) ? "none" : "all 0.05s ubic-bezier(0.1, 2.7, 0.58, 1)"
-}
-
-setInterval(setDate, 1000);
+  hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+  mn.style.transform = `rotateZ(${mm}deg)`;
+  sc.style.transform = `rotateZ(${ss}deg)`;
+});
