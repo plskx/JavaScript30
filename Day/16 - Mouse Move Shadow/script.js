c207c2f4ad0d3fb9a253912d0a04012a9a00e62f
plskz <zaisantillan14@gmail.com>
2021-06-28 23:02:25 +0800

    :art:

--- Day/16 - Mouse Move Shadow/script.js
+++ Day/16 - Mouse Move Shadow/script.js
@@ -4,12 +4,7 @@ const walk = 500; // 500px
 
 function shadow(e) {
   const { offsetWidth: width, offsetHeight: height } = hero;
-  let { offsetX: x, offsetY: y } = e;
-
-  if (this !== e.target) {
-    x += e.target.offsetLeft;
-    y += e.target.offsetTop;
-  }
+  let { clientX: x, clientY: y } = e;
 
   const xWalk = Math.round((x / width) * walk - walk / 2);
   const yWalk = Math.round((y / height) * walk - walk / 2);
