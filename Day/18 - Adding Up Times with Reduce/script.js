ed1ded6bd0bc3e33c5f89f5df9298c5562ea3573
plskz <zaisantillan14@gmail.com>
2021-06-29 23:49:13 +0800

    :sparkles: Day 18

---
+++ Day/18 - Adding Up Times with Reduce/script.js
@@ -0,0 +1,21 @@
+const h1 = document.querySelector(".h1");
+
+const timeNodes = Array.from(document.querySelectorAll("[data-time]"));
+
+const seconds = timeNodes
+  .map((node) => node.dataset.time)
+  .map((timeCode) => {
+    const [mins, secs] = timeCode.split(":").map(parseFloat);
+    return mins * 60 + secs;
+  })
+  .reduce((total, vidSeconds) => total + vidSeconds);
+
+let secondsLeft = seconds;
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft = secondsLeft % 3600;
+
+const mins = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
+
+console.log(hours, mins, secondsLeft);
+h1.textContent = `${hours} hrs ${mins} mins ${secondsLeft} mins left`;
