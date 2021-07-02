c411d2afd0b298b263844e820d5141ea45eda948
plskz <zaisantillan14@gmail.com>
2021-07-02 19:28:46 +0800

    :sparkles: Day 24

---
+++ Day/24 - Sticky Nav/script.js
@@ -0,0 +1,12 @@
+const nav      = document.querySelector("#main");
+const topOfNav = nav.offsetTop;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = `${nav.offsetHeight}px`;
+    document.body.classList.add("fixed-nav");
+  } else {
+    document.body.style.paddingTop = 0;
+    document.body.classList.remove("fixed-nav");
+  }
+});
